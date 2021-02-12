a8c5a8014d062381bc6220b7aae0075b211bd560
Lugita Analia Risanty R <lugitarisanty@gmail.com>
2021-02-12 07:52:59 +0700

    new template

---
+++ js/custom.js
@@ -0,0 +1,51 @@
+/* ----------------------------------
+
+Name: custom.js
+Author: Fourtabs
+Author: TemplateMilk
+Website: https://www.templatemilk.com
+
+------------------------------------- */
+
+
+
+
+
+
+/*====================================================
+
+
+	Table of Contents
+	
+	01. Lightbox
+
+	
+	
+====================================================*/
+
+
+
+jQuery(document).ready(function($) {
+	"use strict";
+
+	
+/*======================
+
+	01. Lightbox 
+
+========================*/
+
+
+
+
+
+	lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true
+    });
+	
+	
+	
+
+	
+});
